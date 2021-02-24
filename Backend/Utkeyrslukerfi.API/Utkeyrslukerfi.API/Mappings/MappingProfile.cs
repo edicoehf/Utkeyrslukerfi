0c15e9a054ad1c14e9f01e6ae8602e1f554e1110
Mikael Máni Jónsson <mikaeelmani99@gmail.com>
2021-02-24 22:30:57 +0000

    Added mapping Profile

--- Backend/Utkeyrslukerfi.API/Utkeyrslukerfi.API/Mappings/MappingProfile.cs
+++ Backend/Utkeyrslukerfi.API/Utkeyrslukerfi.API/Mappings/MappingProfile.cs
@@ -1,9 +1,15 @@
 using AutoMapper;
+using Utkeyrslukerfi.API.Models.Dtos;
+using Utkeyrslukerfi.API.Models.Entities;
 
 namespace Utkeyrslukerfi.API{
     public class MappingProfile : Profile{
         public MappingProfile(){
-            
+            CreateMap<Delivery, DeliveryDTO>();
+            CreateMap<Package, PackageDTO>();
+            CreateMap<User, UserDTO>();
+            CreateMap<Address, AddressDTO>();
+            CreateMap<Vehicle, VehicleDTO>();
         }
     }
 }
