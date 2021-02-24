191922a3fe02245433a4dad10a9d5b037c5b3e99
Margret Soley <margretmsk@gmail.com>
2021-02-24 18:19:21 +0000

    Adding Create Delivery

--- Backend/Utkeyrslukerfi.API/Utkeyrslukerfi.API/Controllers/DeliveryController.cs
+++ Backend/Utkeyrslukerfi.API/Utkeyrslukerfi.API/Controllers/DeliveryController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Utkeyrslukerfi.API.Models.InputModels;
+using Utkeyrslukerfi.API.Services.Interfaces;
 
 namespace Utkeyrslukerfi.API.Controllers
 {
@@ -13,15 +15,26 @@ namespace Utkeyrslukerfi.API.Controllers
     {
 
         private readonly ILogger<DeliveryController> _logger;
+        private readonly IDeliveryService _deliveryService;
 
-        public DeliveryController(ILogger<DeliveryController> logger)
+        public DeliveryController(ILogger<DeliveryController> logger, IDeliveryService deliveryService)
         {
             _logger = logger;
+            _deliveryService = deliveryService;
         }
 
         // get deliveries
         // get delivery
         // update delivery
         // create delivery
+        [HttpPost]
+        [Route("", Name="CreateDelivery")]
+        public IActionResult CreateDelivery([FromBody] DeliveryInputModel delivery){
+            if (!ModelState.IsValid){
+                throw new Exception("Error in CreateDelivery controller");
+            }
+            var new_delivery = _deliveryService.CreateDelivery(delivery);
+            return CreatedAtRoute("CreateDelivery", new_delivery, null);
+        }
     }
 }
