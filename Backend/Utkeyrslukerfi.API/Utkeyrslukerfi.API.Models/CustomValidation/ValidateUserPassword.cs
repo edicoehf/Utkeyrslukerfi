792f0f982d153b7f499d5923dd6fb6ab7c60fafc
Margret Soley <margretmsk@gmail.com>
2021-03-03 18:16:02 +0000

    Fixing so password cannot be empty

--- Backend/Utkeyrslukerfi.API/Utkeyrslukerfi.API.Models/CustomValidation/ValidateUserPassword.cs
+++ Backend/Utkeyrslukerfi.API/Utkeyrslukerfi.API.Models/CustomValidation/ValidateUserPassword.cs
@@ -4,10 +4,10 @@ namespace Utkeyrslukerfi.API.Models {
     public class ValidateUserPassword:ValidationAttribute {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext) {
             var model = (Models.InputModels.UserInputModel)validationContext.ObjectInstance;
-            var password = value;
+            string password = value as string;
             var role = model.Role;  
 
-            if (role != 3 && password == null) {
+            if (role != 3 && string.IsNullOrEmpty(password)) {
                 return new ValidationResult("User must have a password.");                
             }
             else {
