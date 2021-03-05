e8ee88f41d72ff567c5b483b8a9e7dfdbc41fba5
ertolv <re_pel@outlook.com>
2021-03-05 00:42:11 +0000

    vehicles

---
+++ Backend/Utkeyrslukerfi.API/Utkeyrslukerfi.API.Repositories/Implementations/VehicleRepository.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Utkeyrslukerfi.API.Models.Dtos;
+using Utkeyrslukerfi.API.Models.Entities;
+using Utkeyrslukerfi.API.Models.InputModels;
+using Utkeyrslukerfi.API.Repositories.Context;
+using Utkeyrslukerfi.API.Repositories.Interfaces;
+
+namespace Utkeyrslukerfi.API.Repositories.Implementations
+{
+    public class VehicleRepository : IVehicleRepository
+    {
+        private readonly UtkeyrslukerfiDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public VehicleRepository(IMapper mapper, UtkeyrslukerfiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+
+
+        public VehicleDTO GetVehicle(string ID)
+        {
+            var entity = _dbContext.Vehicles.FirstOrDefault(v => v.LicensePlate == ID);
+            if (entity == null) { return null; }
+            return _mapper.Map<VehicleDTO>(entity);
+        }
+
+        public VehicleDTO CreateVehicle(VehicleInputModel vehicle)
+        {
+            var entity = new Vehicle
+            {
+                // vehicle properties
+            };
+            _dbContext.Vehicles.Add(entity);
+            _dbContext.SaveChanges();
+            return null;
+        }
+
+        public void UpdateVehicle(VehicleInputModel vehicle, string id)
+        {
+            // TODO:
+            // _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<VehicleDTO> GetVehicles()
+        {
+            var vehicles = _dbContext.Vehicles.ToList();
+
+            // TODO: 
+
+            // return _mapper.Map<IEnumerable<VehiclesDTO>>(vehicles);
+            return null;
+        }
+
+    }
+}
