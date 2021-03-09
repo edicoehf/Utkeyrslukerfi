e1af6f442a938e375001fd0f064ce71d7ccb8aad
Mikael Máni Jónsson <mikaeelmani99@gmail.com>
2021-03-09 13:11:04 +0000

    Initial commit for web frontend

--- Backend/Utkeyrslukerfi.API/Utkeyrslukerfi.API/Controllers/UserController.cs
+++ Backend/Utkeyrslukerfi.API/Utkeyrslukerfi.API/Controllers/UserController.cs
@@ -9,59 +9,63 @@ using Utkeyrslukerfi.API.Services.Interfaces;
 
 namespace Utkeyrslukerfi.API.Controllers
 {
-    [ApiController]
-    [Route("api/users")]
-    public class UserController : ControllerBase
+  [ApiController]
+  [Route("api/users")]
+  public class UserController : ControllerBase
+  {
+
+    private readonly ILogger<UserController> _logger;
+    private readonly IUserService _userService;
+
+    public UserController(ILogger<UserController> logger, IUserService userService)
     {
-
-        private readonly ILogger<UserController> _logger;
-        private readonly IUserService _userService;
-
-        public UserController(ILogger<UserController> logger, IUserService userService)
-        {
-            _logger = logger;
-            _userService = userService;
-        }
-        /// <summary>
-        /// Returns a specific delivery by ID/Barcode
-        /// </summary>
-        /// <param name="id">ID</param>
-        /// <remarks>
-        /// Sample request:
-        ///
-        ///     GET api/users/1
-        ///     {
-        ///       "ID": 1,
-        ///       "Name": "Mikael Máni Jónsson",
-        ///       "Email": "Mikaeelmani99@gmail.com",
-        ///       "Role": 3
-        ///     }
-        ///
-        /// </remarks>
-        /// <returns>A user with the given ID</returns>
-        /// <response code="200">Returns the user with the given ID</response>
-        /// <response code="401">The Auth token was invalid </response>
-        /// <response code="404">There is no user with the given ID</response> 
-        [HttpGet]
-        [Route("{id:int}")]
-        public IActionResult getUser(int id){
-            var user = _userService.GetUser(id);
-            return Ok(user);
-        }
-
-        [HttpGet]
-        public IActionResult getUsers(){
-            return NoContent();
-        }
-
-        [HttpPost]
-        public IActionResult RegisterUser([FromBody] UserInputModel inputModel){
-            return NoContent();
-        }
-
-        [HttpPut]
-        public IActionResult UpdateUser([FromBody] UserInputModel inputModel){
-            return NoContent();
-        }
+      _logger = logger;
+      _userService = userService;
     }
+    /// <summary>
+    /// Returns a specific delivery by ID/Barcode
+    /// </summary>
+    /// <param name="id">ID</param>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     GET api/users/1
+    ///     {
+    ///       "ID": 1,
+    ///       "Name": "Mikael Máni Jónsson",
+    ///       "Email": "Mikaeelmani99@gmail.com",
+    ///       "Role": 3
+    ///     }
+    ///
+    /// </remarks>
+    /// <returns>A user with the given ID</returns>
+    /// <response code="200">Returns the user with the given ID</response>
+    /// <response code="401">The Auth token was invalid </response>
+    /// <response code="404">There is no user with the given ID</response> 
+    [HttpGet]
+    [Route("{id:int}")]
+    public IActionResult getUser(int id)
+    {
+      var user = _userService.GetUser(id);
+      return Ok(user);
+    }
+
+    [HttpGet]
+    public IActionResult getUsers()
+    {
+      return NoContent();
+    }
+
+    [HttpPost]
+    public IActionResult RegisterUser([FromBody] UserInputModel inputModel)
+    {
+      return NoContent();
+    }
+
+    [HttpPut]
+    public IActionResult UpdateUser([FromBody] UserInputModel inputModel)
+    {
+      return NoContent();
+    }
+  }
 }
