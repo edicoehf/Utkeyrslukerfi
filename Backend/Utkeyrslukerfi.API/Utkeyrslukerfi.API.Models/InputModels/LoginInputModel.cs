025bca6567667c1d457073aa8bf0a9441df4b2f5
Margret Soley <margretmsk@gmail.com>
2021-03-10 16:07:03 +0000

    Adding account controller and logininputmodel

--- Backend/Utkeyrslukerfi.API/Utkeyrslukerfi.API.Models/InputModels/LoginInputModel.cs
+++ Backend/Utkeyrslukerfi.API/Utkeyrslukerfi.API.Models/InputModels/LoginInputModel.cs
@@ -2,15 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Utkeyrslukerfi.API.Models.InputModels
 {
-    public class UserInputModel
+    public class LoginInputModel
     {
-        [Required(ErrorMessage = "Name is required")]
-        public string Name { get; set; }
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Has to be a valid Email")]
         public string Email { get; set; }
-        [Required(ErrorMessage = "Role is required")] // Role 1: admin, 2: office, 3: driver
-        public int Role { get; set; }
         [ValidateUserPassword]
         public string Password { get; set; }
 
