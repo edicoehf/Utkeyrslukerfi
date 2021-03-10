6a452a579490ab86785b20e0e836a50118eda994
Margret Soley <margretmsk@gmail.com>
2021-03-10 14:29:48 +0000

    Fixing cuz of linter

--- Web/utkeyrslukerfi-web/src/App.js
+++ Web/utkeyrslukerfi-web/src/App.js
@@ -1,12 +1,12 @@
 import './App.css'
-import { Switch, Route } from 'react-router-dom';
-import Users from './views/Users';
+import { Switch, Route } from 'react-router-dom'
+import Users from './views/Users'
 
 function App () {
   return (
     <div className='App'>
       <Switch>
-        <Route exact path='/users' component={ Users }/>
+        <Route exact path='/users' component={Users} />
       </Switch>
     </div>
   )
