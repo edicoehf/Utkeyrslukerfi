e698978abf375b78b9b31e8ec549bcb4ef30a8f8
Mikael Máni Jónsson <mikaeelmani99@gmail.com>
2021-03-15 15:18:41 +0000

    Added Delivery skeleton

---
+++ Web/utkeyrslukerfi-web/src/views/Deliveries/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import { connect } from 'react-redux'
+
+const Deliveries = () => {
+  return (
+    <div>
+      something
+    </div>
+  )
+}
+
+export default Deliveries
+
+// const mapStateToProps = reduxStoreState => {
+//   return {
+//     deliveries: reduxStoreState.deliveries
+//   }
+// }
+
+// export default connect(mapStateToProps, getDeliveries)(Users)
