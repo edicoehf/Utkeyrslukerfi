194d1421afa2be6759903427db3366f5761640f9
Margret Soley <margretmsk@gmail.com>
2021-03-15 20:41:24 +0000

    Just commenting out to run

--- Web/utkeyrslukerfi-web/src/App.js
+++ Web/utkeyrslukerfi-web/src/App.js
@@ -2,10 +2,10 @@ import './styles/navbar.css'
 import './styles/main.css'
 import { Switch, Route } from 'react-router-dom'
 import Users from './views/Users'
-import Deliveries from './views/Deliveries'
+// import Deliveries from './views/Deliveries'
 import Navbar from './components/Navbar'
 import Container from './components/Container'
-import CreateUserForm from './components/CreateUserForm'
+import CreateUserForm from './views/CreateUserForm'
 
 function App () {
   return (
@@ -15,7 +15,7 @@ function App () {
         <Switch>
           <Route exact path='/users' component={Users} />
           <Route exact path='/users/create' component={CreateUserForm} />
-          <Route exact path='/deliveries' component={Deliveries} />
+          {/* <Route exact path='/deliveries' component={Deliveries} /> */}
         </Switch>
       </Container>
     </div>
