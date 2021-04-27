72dac85ac035f7a40c2ed1bd753f63732a2956fd
joelsnaer <joelsnaer1999@hotmail.com>
2021-04-27 11:18:32 +0000

    Logging in using app service complete, actual login page left

--- Handheld/UtkeyrslukerfiHandheld/src/reducers/index.js
+++ Handheld/UtkeyrslukerfiHandheld/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import statusCode from './statusCodeReducer'
+import login from './loginReducer'
 
 export default combineReducers({
-  statusCode
+  statusCode,
+  login
 })
