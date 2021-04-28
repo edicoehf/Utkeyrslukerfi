1e597cc4e2f7cbdc62ea9e514f9d7150e4e95d52
Margret Soley <margretmsk@gmail.com>
2021-04-28 21:31:03 +0000

    rm-ing useless imports

--- Handheld/UtkeyrslukerfiHandheld/src/views/DeliveryReceivedScreen/index.js
+++ Handheld/UtkeyrslukerfiHandheld/src/views/DeliveryReceivedScreen/index.js
@@ -1,10 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import { View, Text, Button } from 'react-native'
-import BarcodeForm from '../../components/BarcodeForm'
-import CommentBox from '../../components/CommentBox'
-import ProductTable from '../../components/ProductTable'
-import RemoveButton from '../../components/RemoveButton'
-import CheckBox from '@react-native-community/checkbox'
 
 // To confirm to the driver that the delivery has been received, there is nothing left to do
 const DeliveryReceivedScreen = ({ navigation }) => {
