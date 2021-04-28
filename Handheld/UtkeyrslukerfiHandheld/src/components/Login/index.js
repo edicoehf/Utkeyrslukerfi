466dc9f48895d58740db73d51534a9c47cd4e1ba
joelsnaer <joelsnaer1999@hotmail.com>
2021-04-28 09:52:15 +0000

    Fixed linter issues and removed test code again

--- Handheld/UtkeyrslukerfiHandheld/src/components/Login/index.js
+++ Handheld/UtkeyrslukerfiHandheld/src/components/Login/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Button, Text } from 'react-native'
 import { setLogin } from '../../actions/loginActions'
 import { useDispatch, useSelector } from 'react-redux'
