d0e4074cee08389fedf1902097248cfbbc9f99af
ertolv <re_pel@outlook.com>
2021-05-01 21:52:39 +0000

    changed vehiclde dto id to guid

--- Backend/Utkeyrslukerfi.API/Utkeyrslukerfi.API.Models/DTOs/VehicleDTO.cs
+++ Backend/Utkeyrslukerfi.API/Utkeyrslukerfi.API.Models/DTOs/VehicleDTO.cs
@@ -1,7 +1,9 @@
+using System;
 namespace Utkeyrslukerfi.API.Models.Dtos
 {
     public class VehicleDTO
     {
+        public Guid ID { get; set; }
         public string LicensePlate { get; set; }
         public double Length { get; set; }
         public double Height { get; set; }
