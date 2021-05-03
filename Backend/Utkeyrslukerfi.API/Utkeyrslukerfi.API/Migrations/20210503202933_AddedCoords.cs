60ee938ac85e8fa2200b59cadfaa716ff553e6bf
Mikael Máni Jónsson <mikaeelmani99@gmail.com>
2021-05-03 20:31:02 +0000

    added Coords to Addresses

---
+++ Backend/Utkeyrslukerfi.API/Utkeyrslukerfi.API/Migrations/20210503202933_AddedCoords.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Utkeyrslukerfi.API.Migrations
+{
+    public partial class AddedCoords : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Deliveries",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "XCoords",
+                table: "Addresses",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "YCoords",
+                table: "Addresses",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "XCoords",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "YCoords",
+                table: "Addresses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Deliveries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
