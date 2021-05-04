f85f409e9a907ecc120a6fe6e0bc62d85d77237d
ertolv <re_pel@outlook.com>
2021-05-04 12:42:40 +0000

    styling v0.3

--- Handheld/UtkeyrslukerfiHandheld/src/components/DeliveryTable/index.js
+++ Handheld/UtkeyrslukerfiHandheld/src/components/DeliveryTable/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Text, View, FlatList, TouchableOpacity } from 'react-native'
+import { useSelector } from 'react-redux'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import styles from '../../styles/deliveryTable'
 import _ from 'lodash'
@@ -7,6 +8,7 @@ import _ from 'lodash'
 const DeliveryTable = ({ data }) => {
   const columns = ['id', 'status', 'date']
   const columnText = ['Sendingarnúmer', 'Staða', 'Dagsetning']
+  const availableStatusCodes = useSelector(({ statusCode }) => statusCode)
   const [direction, setDirection] = useState(null)
   const [selectedColumn, setSelectedColumn] = useState(null)
   const [deliveries, setDeliveries] = useState()
@@ -23,26 +25,27 @@ const DeliveryTable = ({ data }) => {
   const tableHeader = () => (
     <View style={styles.tableHeader}>
       {
-              columns.map((column, index) => {
-                return (
-                  <TouchableOpacity
-                    key={index}
-                    style={styles.columnHeader}
-                    onPress={() => sortTable(column)}
-                  >
-                    <Text style={styles.columnHeaderTxt}>{columnText[index] + ' '}
-                      {selectedColumn === column && <Icon
-                        name={direction === 'desc' ? 'arrow-down-drop-circle' : 'arrow-up-drop-circle'}
-                                                    />}
-                    </Text>
-                  </TouchableOpacity>
-                )
-              })
-          }
+        columns.map((column, index) => {
+          return (
+            <TouchableOpacity
+              key={index}
+              style={styles.columnHeader}
+              onPress={() => sortTable(column)}
+            >
+              <Text style={styles.columnHeaderTxt}>{columnText[index] + ' '}
+                {selectedColumn === column && <Icon
+                  name={direction === 'desc' ? 'arrow-down-drop-circle' : 'arrow-up-drop-circle'}
+                />}
+              </Text>
+            </TouchableOpacity>
+          )
+        })
+      }
     </View>
   )
 
   const tableBody = () => {
+
     // TODO: Laga þessa þvælu
     if (counter <= 0) {
       setDeliveries(data)
@@ -59,7 +62,7 @@ const DeliveryTable = ({ data }) => {
           return (
             <View style={{ ...styles.tableRow, backgroundColor: index % 2 === 1 ? '#F0FBFC' : 'white' }}>
               <Text style={{ ...styles.columnRowTxt, fontWeight: 'bold' }}>{item.id}</Text>
-              <Text style={styles.columnRowTxt}>{item.status}</Text>
+              <Text style={styles.columnRowTxt}>{availableStatusCodes[item.status]}</Text>
               <Text style={styles.columnRowTxt}>{item.pickupAddress.city}</Text>
             </View>
           )
@@ -70,8 +73,8 @@ const DeliveryTable = ({ data }) => {
   return (
     <View style={styles.container}>
       {
-                data.length > 0 ? tableBody() : null
-            }
+        data.length > 0 ? tableBody() : null
+      }
     </View>
   )
 }
---
+++ Handheld/UtkeyrslukerfiHandheld/src/styles/searchPageStyles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  mainView: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  section: {
+    marginBottom: 15,
+    marginHorizontal: 5,
+    padding: 5,
+    borderRadius: 3,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  mainText: {
+    fontSize: 20,
+    paddingLeft: 1,
+  }
+});
