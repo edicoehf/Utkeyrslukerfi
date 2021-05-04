3d03772ebd6c76dc60737b37e959fd2e57612dfe
MikaelMani99 <mikaeelmani99@gmail.com>
2021-05-04 16:37:16 +0000

    refactered, made it more readable and follows the single responsability more

---
+++ Web/utkeyrslukerfi-web/src/components/DeliveryDetails/index.jsx
@@ -0,0 +1,33 @@
+import { useDispatch } from "react-redux"
+import { useHistory } from "react-router"
+import { setDelivery } from '../../actions/deliveryActions'
+import { BsPencilSquare } from 'react-icons/bs'
+import { format } from 'date-fns'
+import configData from '../../constants/config.json'
+
+const DeliveryDetails = ({delivery}) => {
+
+    const dispatch = useDispatch()
+    const history = useHistory()
+
+    const navigateToDelivery = (delivery) => {
+        dispatch(setDelivery(delivery))
+        history.push(`/deliveries/${delivery.id}`)
+    }
+    console.log("halló")
+    return (
+        <tr key={delivery.id}>
+          <td>{delivery.id}</td>
+          <td>{configData.STATUS[delivery.status]}</td>
+          <td>{delivery.recipient}</td>
+          <td>{delivery.seller === null ? 'N/A' : delivery.seller}</td>
+          <td>{delivery.driver === null ? 'N/A' : delivery.driver.name}</td>
+          <td>{delivery.deliveryDate === null ? 'N/A' : format(new Date(delivery.deliveryDate), 'MMMM do, yyyy')}</td>
+          <td>{delivery.deliveryAddress.streetName} {delivery.deliveryAddress.houseNumber}</td>
+          <td>{delivery.pickupAddress.streetName} {delivery.pickupAddress.houseNumber}</td>
+          <td onClick={() => navigateToDelivery(delivery)} className='clickable'><BsPencilSquare size='1.5em' /></td>
+        </tr>
+    )
+}
+
+export default DeliveryDetails
--- Web/utkeyrslukerfi-web/src/views/DeliveriesList/index.jsx
+++ Web/utkeyrslukerfi-web/src/views/DeliveriesList/index.jsx
@@ -1,18 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { useHistory } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { getDeliveries } from '../../actions/deliveriesActions'
-import { setDelivery } from '../../actions/deliveryActions'
 import { getDrivers } from '../../actions/usersActions'
 import { getVehicles } from '../../actions/vehiclesActions'
-import configData from '../../constants/config.json'
-import { BsPencilSquare, BsFunnel } from 'react-icons/bs'
+import { BsFunnel } from 'react-icons/bs'
 import '../../styles/deliveries.css'
 import DeliveryFilterModal from '../../components/DeliveryFilterModal'
-import { format } from 'date-fns'
+import DeliveryDetails from '../../components/DeliveryDetails'
 
 const DeliveriesList = () => {
-  const history = useHistory()
   const token = useSelector(({ login }) => login.token)
   const deliveries = useSelector(({ deliveries }) => deliveries)
   const [deliveryState, setDeliveryState] = useState([])
@@ -32,61 +28,24 @@ const DeliveriesList = () => {
     setDeliveryState(deliveries)
   }, [deliveries])
 
-  const navigateToDelivery = (obj) => {
-    dispatch(setDelivery(obj))
-    history.push(`/deliveries/${obj.id}`)
-  }
-
   const toggleModal = () => {
     setFilterModel(state => !state)
   }
 
-  const renderRows = () => {
-    return deliveryState.map(function (obj, id) {
-      return (
-        <tr key={id}>
-          <td>{obj.id}</td>
-          <td>{configData.STATUS[obj.status]}</td>
-          <td>{obj.recipient}</td>
-          <td>{obj.seller === null ? 'N/A' : obj.seller}</td>
-          <td>{obj.driver === null ? 'N/A' : obj.driver.name}</td>
-          <td>{obj.deliveryDate === null ? 'N/A' : format(new Date(obj.deliveryDate), 'MMMM do, yyyy')}</td>
-          <td>{obj.deliveryAddress.streetName} {obj.deliveryAddress.houseNumber}</td>
-          <td>{obj.pickupAddress.streetName} {obj.pickupAddress.houseNumber}</td>
-          <td onClick={() => navigateToDelivery(obj)} className='clickable'><BsPencilSquare size='1.5em' /></td>
-        </tr>
-      )
-    })
-  }
   return (
     <div className='deliveries'>
       <table className='table table-bordered'>
         <thead className='thead-dark'>
           <tr>
-            <th>
-              ID
+            <th>ID</th>
+            <th>Staða</th>
+            <th>Móttakandi</th>
+            <th>Sendandi</th>
+            <th>Bílstjóri</th>
+            <th>Sendingar dagsetning
             </th>
-            <th>
-              Staða
-            </th>
-            <th>
-              Móttakandi
-            </th>
-            <th>
-              Sendandi
-            </th>
-            <th>
-              Bílstjóri
-            </th>
-            <th>
-              Sendingar dagsetning
-            </th>
-            <th>
-              Heimilisfang móttakanda
-            </th>
-            <th>
-              Heimilisfang sendanda
-            </th>
+            <th>Heimilisfang móttakanda</th>
+            <th>Heimilisfang sendanda</th>
             <th onClick={toggleModal} className='clickable'>
               {/* Edit pen icon */}
               <BsFunnel size='2em' color='white' />
@@ -95,16 +54,21 @@ const DeliveriesList = () => {
         </thead>
         <tbody>
           {
-            deliveries.length > 0 ? renderRows() : null
+            deliveryState.length > 0 
+            ? 
+            deliveryState.map((deliv) => (
+              <DeliveryDetails delivery={deliv} />
+            )) 
+            : null
           }
         </tbody>
       </table>
       {
         deliveries.length <= 0
           ? <div className='text-center'>
-            <div className='spinner-border' role='status'>
-              <span className='sr-only'>Loading...</span>
-            </div>
+              <div className='spinner-border' role='status'>
+                <span className='sr-only'>Loading...</span>
+              </div>
           </div>
           : null
       }
