26b50385faf99a39f504cb06d169833e774d3288
Margret Soley <margretmsk@gmail.com>
2021-05-04 11:40:20 +0000

    Some css signature screen

--- Handheld/UtkeyrslukerfiHandheld/src/views/SignForDeliveryScreen/index.js
+++ Handheld/UtkeyrslukerfiHandheld/src/views/SignForDeliveryScreen/index.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { View, Text, ToastAndroid, TextInput } from 'react-native'
 import Signature from 'react-native-signature-canvas'
 import { useDispatch, useSelector } from 'react-redux'
 import { setStep } from '../../actions/signingProcessActions'
+import BasicButton from '../../components/BasicButton'
+import styles from '../../styles/signForDeliveryScreen'
 
 // Let the receiver sign for the delivery
 const SignForDeliveryScreen = ({ navigation }) => {
@@ -11,6 +13,7 @@ const SignForDeliveryScreen = ({ navigation }) => {
   const [name, setName] = useState('')
   const signingProcess = useSelector(({ signingProcess }) => signingProcess)
   const dispatch = useDispatch()
+  const canvasRef = useRef()
 
   const handleSignature = (signature) => {
     // TODO: save the image on a server and save the link in the db, get the link for the image from signature.data
@@ -26,19 +29,39 @@ const SignForDeliveryScreen = ({ navigation }) => {
 
   const handleEmpty = () => { ToastAndroid.showWithGravity('Undirskrift vantar.', ToastAndroid.LONG, ToastAndroid.TOP) }
 
+  const handleClear = () => { canvasRef.current.clearSignature() }
+
+  const handleConfirm = () => { canvasRef.current.readSignature() }
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Nafn móttakanda:</Text>
+    <View style={styles.container}>
+      <Text style={styles.label}>Nafn móttakanda:</Text>
       <TextInput
+        style={styles.input}
         onChangeText={setName}
       />
-      <Signature
-        onOK={handleSignature}
-        onEmpty={handleEmpty}
-        descriptionText='Undirskrift móttakanda'
-        clearText='Hreinsa'
-        confirmText='Vista'
-      />
+      <View style={styles.canvasContainer}>
+        <Signature
+          ref={canvasRef}
+          style={styles.canvas}
+          onOK={handleSignature}
+          onEmpty={handleEmpty}
+          bgColor={"#000000"}
+          webStyle={`
+          .m-signature-pad--footer {display: none !important;}
+          `}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <BasicButton 
+          buttonText='Hreinsa'
+          onPressFunction={handleClear}
+        />
+        <BasicButton
+          buttonText='Vista'
+          onPressFunction={handleConfirm}
+        />
+      </View>
     </View>
   )
 }
