420ef9cc73970ecbb99afaed92c26b82582a5bab
Margret Soley <margretmsk@gmail.com>
2021-05-04 10:12:14 +0000

    css image screen

---
+++ Handheld/UtkeyrslukerfiHandheld/src/styles/imageOnDeliveryScreen.js
@@ -0,0 +1,47 @@
+import { Dimensions, StyleSheet } from 'react-native'
+
+const windowHeight = Dimensions.get('window').height
+const windowWidth = Dimensions.get('window').width
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    alignItems: 'center'
+  },
+  image: {
+    width: windowWidth * 0.9,
+    height: windowWidth * 0.9,
+    overflow: 'hidden',
+    borderColor: '#cccccc',
+    borderWidth: windowWidth * 0.005,
+    borderRadius: 5,
+    margin: windowHeight * 0.05
+  },
+  input: {
+    backgroundColor: '#ffffff',
+    height: windowHeight * 0.05,
+    width: windowWidth * 0.50,
+    borderColor: '#cccccc',
+    borderWidth: 0.5,
+    borderRadius: 5,
+    marginBottom: windowWidth * 0.02
+  },
+  label: {
+    width: windowWidth * 0.50,
+    justifyContent: 'flex-start',
+    marginTop: windowHeight * 0.05
+  },
+  buttonContainer: {
+    height: windowHeight * 0.10,
+    width: windowWidth * 0.80,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: '2%',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: windowHeight * 0.03,
+    left: windowWidth * 0.1
+  }
+})
+
+export default styles
--- Handheld/UtkeyrslukerfiHandheld/src/views/ImageOnDeliveryScreen/index.js
+++ Handheld/UtkeyrslukerfiHandheld/src/views/ImageOnDeliveryScreen/index.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
-import { View, Text, ToastAndroid, TextInput, ImageBackground } from 'react-native'
+import { View, Text, ToastAndroid, TextInput, ImageBackground, TouchableOpacity } from 'react-native'
 import { launchCamera } from 'react-native-image-picker'
 import { useDispatch, useSelector } from 'react-redux'
 import { setStep } from '../../actions/signingProcessActions'
 import BasicButton from '../../components/BasicButton'
+import styles from '../../styles/imageOnDeliveryScreen'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 
 // Let the receiver sign for the delivery
 const ImageOnDeliveryScreen = ({ navigation }) => {
@@ -15,7 +17,7 @@ const ImageOnDeliveryScreen = ({ navigation }) => {
   const [imageSource, setImageSource] = useState(null)
 
   const continueWithDelivery = () => {
-    // TODO: save the image on a server and save the link in the db, get the link for the image from signature.data
+    // TODO: save the image on a server and save the link in the db, get the link for the image from imageSource
     if (!name) {
       ToastAndroid.showWithGravity('Vinsamlegast settu inn nafn móttakanda.', ToastAndroid.LONG, ToastAndroid.TOP)
       return
@@ -47,15 +49,20 @@ const ImageOnDeliveryScreen = ({ navigation }) => {
   }
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Nafn móttakanda:</Text>
+    <View style={styles.container}>
+      <Text style={styles.label}>Nafn móttakanda:</Text>
       <TextInput
+        style={styles.input}
         onChangeText={setName}
       />
-      { !imageSource && <ImageBackground style={{width: 300, height: 300, overflow: 'hidden'}} source={ require('../../images/no_image_available.jpg') } />}
-      { imageSource && <ImageBackground style={{width: 300, height: 300, overflow: 'hidden'}} source={ {uri: imageSource} } /> }
-      <BasicButton buttonText='Taka mynd' onPressFunction={takeImage} />
-      <BasicButton buttonText='Vista' onPressFunction={continueWithDelivery} />
+      { !imageSource && <ImageBackground style={styles.image} source={ require('../../images/no_image_available.jpg') } />}
+      { imageSource && <ImageBackground style={styles.image} source={ {uri: imageSource} } /> }
+      <View style={styles.buttonContainer} >
+        <TouchableOpacity onPress={takeImage}>
+          <FontAwesome name='camera' style={{ width: 26 - 32 }} color='#333' size={35} />
+        </TouchableOpacity>
+        <BasicButton buttonText='Vista' onPressFunction={continueWithDelivery} />
+      </View>
     </View>
   )
 }
