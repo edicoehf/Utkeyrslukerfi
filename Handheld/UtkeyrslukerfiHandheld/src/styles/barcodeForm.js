d2c0342cbcc281053d4f85b07718db51d8116094
ertolv <re_pel@outlook.com>
2021-05-05 12:59:12 +0000

    styling

--- Handheld/UtkeyrslukerfiHandheld/src/styles/barcodeForm.js
+++ Handheld/UtkeyrslukerfiHandheld/src/styles/barcodeForm.js
@@ -11,6 +11,10 @@ const styles = StyleSheet.create({
     paddingTop: '2%',
     width: windowWidth * 0.70
   },
+  inputContainer: {
+    marginRight: 3,
+    marginTop: 1
+  },
   input: {
     backgroundColor: '#ffffff',
     height: windowHeight * 0.06,
@@ -26,7 +30,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 5,
-    margin: '2%',
+    margin: 3,
     alignSelf: 'flex-end',
     marginTop: windowHeight * 0.025
   },
