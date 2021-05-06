d0321efd564845fa19a44058df22913ce316bca4
ertolv <re_pel@outlook.com>
2021-05-06 09:28:16 +0000

    commenting

--- Backend/Utkeyrslukerfi.API/Utkeyrslukerfi.API.Repositories/Implementations/AddressRepository.cs
+++ Backend/Utkeyrslukerfi.API/Utkeyrslukerfi.API.Repositories/Implementations/AddressRepository.cs
@@ -19,18 +19,24 @@ namespace Utkeyrslukerfi.API.Repositories.Implementations
             _dbContext = dbContext;
             _mapper = mapper;
         }
+        /// <summary>
+        /// Find the address with the given ID
+        /// </summary>
+        /// <param name="ID">ID of the Address (Required).</param>
+        /// <returns>An Address DTO</returns>
         public AddressDTO GetAddress(Guid ID)
         {
-            // Find the address with the given ID
             var entity = _dbContext.Addresses.FirstOrDefault(a => a.ID == ID);
-            // If it does not exist, return null, throws not found exception
             if (entity == null) { throw new NotFoundException($"No Address with ID: {ID}"); }
-            // If exists then map the address according to DTO and return.
             return _mapper.Map<AddressDTO>(entity);
         }
+        /// <summary>
+        /// Creates a new address
+        /// </summary>
+        /// <param name="address">Address object that is going to be created (Requried).</param>
+        /// <returns>Return the ID of the address that was created </returns>
         public Guid CreateAddress(AddressInputModel address)
         {
-            // Create a new address entity
             var entity = new Address
             {
                 StreetName = address.StreetName,
@@ -39,11 +45,8 @@ namespace Utkeyrslukerfi.API.Repositories.Implementations
                 Country = address.Country,
                 City = address.City
             };
-            // Add newly created address entity at database
             _dbContext.Addresses.Add(entity);
-            // Finally save the changes that were made in database
             _dbContext.SaveChanges();
-            // Return the id of the entity that was created 
             return entity.ID;
         }
     }
