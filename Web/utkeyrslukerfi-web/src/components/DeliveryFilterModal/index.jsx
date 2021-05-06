afb093ad230a1fd4d228607ce7bbf27e8a1b2feb
ertolv <re_pel@outlook.com>
2021-05-06 18:29:16 +0000

    temp, needs fix

--- Web/utkeyrslukerfi-web/src/components/DeliveryFilterModal/index.jsx
+++ Web/utkeyrslukerfi-web/src/components/DeliveryFilterModal/index.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Modal from 'react-modal'
 import config from '../../constants/config.json'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 import { isWithinInterval } from 'date-fns'
+import FormGroupDropdown from '../FormGroupDropdown'
+import Form from 'react-bootstrap/Form'
+import { useForm, FormProvider } from 'react-hook-form'
 import configData from '../../constants/config.json'
 
 const customStyles = {
@@ -24,12 +27,13 @@ const UserFilterModal = ({ visible, deliveries, setDeliveries, deliveryState, up
   const [status, setStatus] = useState('')
   const [startDate, setStartDate] = useState(new Date(Date.now() - (6.048e+8)))
   const [endDate, setEndDate] = useState(new Date(Date.now() + (6.048e+8 * 2)))
+  const methods = useForm()
 
   function getIDByStatus(status) {
     return Object.keys(config.STATUS).find(key => config.STATUS[key] === status)
   }
 
-  const filter = () => {
+  const filter = (e) => {
     updateModalState()
     setDeliveries([])
     setDeliveries(deliveries.filter(d => isWithinInterval(new Date(d.deliveryDate), { start: startDate, end: endDate })))
@@ -48,6 +52,10 @@ const UserFilterModal = ({ visible, deliveries, setDeliveries, deliveryState, up
     filter()
   }
 
+  useEffect(() => {
+    methods.setValue('status', deliveryState)
+  })
+
   if (visible) {
     return (
       <Modal
@@ -57,57 +65,63 @@ const UserFilterModal = ({ visible, deliveries, setDeliveries, deliveryState, up
         style={customStyles}
       >
         <h2>Sía sendingar</h2>
-        <form onSubmit={submitHandler}>
-          <div className='row pb-3'>
-            <div className='col float-right'>
-              <p className='my-auto'>Staða</p>
+        <FormProvider {...methods}>
+          <Form onSubmit={methods.handleSubmit(submitHandler)}>
+            <div className='row pb-3'>
+              <div className='col align-self-center'>
+                <FormGroupDropdown
+                  groupType='status'
+                  label='Staða'
+                  options={
+                    <>
+                      {Object.keys(configData.STATUS).map(function (key) {
+                        return (
+                          <option key={key} value={key}>{configData.STATUS[key]}</option>
+                        )
+                      })}
+                    </>
+                  }
+                  typeOfForm='UserFilterModal'
+                />
+              </div>
             </div>
-            <div className='col align-self-center'>
-              <select id='status' name='status' onChange={event => setStatus(event.target.value)}>
-                {Object.keys(configData.STATUS).map(function (key) {
-                  return (
-                    <option key={key} value={key}>{configData.STATUS[key]}</option>
-                  )
-                })}
-              </select>
+            <div className='row'>
+              <div className='col pb-3'>
+                <span className='my-auto'>Frá</span>
+              </div>
+              <div className='col align-self-start'>
+                <DatePicker
+                  selected={startDate}
+                  onChange={date => setStartDate(date)}
+                  selectsStart
+                  startDate={startDate}
+                  endDate={endDate}
+                />
+              </div>
             </div>
-          </div>
-          <div className='row'>
-            <div className='col pb-3'>
-              <p className='my-auto'>Frá</p>
+            <div className='row'>
+              <div className='col align-self-start'>
+                <p className='my-auto'>Til</p>
+              </div>
+              <div className='col align-self-start'>
+                <DatePicker
+                  selected={endDate}
+                  onChange={date => {
+                    date.setMinutes(date.getMinutes() + 30)
+                    setEndDate(date)
+                  }}
+                  selectsEnd
+                  startDate={startDate}
+                  endDate={endDate}
+                  minDate={startDate}
+                />
+              </div>
             </div>
-            <div className='col align-self-start'>
-              <DatePicker
-                selected={startDate}
-                onChange={date => setStartDate(date)}
-                selectsStart
-                startDate={startDate}
-                endDate={endDate}
-              />
-            </div>
-          </div>
-          <div className='row'>
-            <div className='col align-self-start'>
-              <p className='my-auto'>Til</p>
-            </div>
-            <div className='col align-self-start'>
-              <DatePicker
-                selected={endDate}
-                onChange={date => {
-                  date.setMinutes(date.getMinutes() + 30)
-                  setEndDate(date)
-                }}
-                selectsEnd
-                startDate={startDate}
-                endDate={endDate}
-                minDate={startDate}
-              />
-            </div>
-          </div>
-        </form>
+          </Form>
+        </FormProvider>
         <div className='row pt-3'>
           <div className='col'>
-            <button className='btn btn-primary' onClick={filter}>Filter</button>
+            <button className='btn btn-primary' onClick={filter}>Sía</button>
           </div>
           <div className='col align-self-end'>
             <button onClick={clearFilter} className='btn btn-outline-warning float-right mx-2'>Hreinsa síu</button>
--- Web/utkeyrslukerfi-web/src/reducers/deliveryReducer.jsx
+++ Web/utkeyrslukerfi-web/src/reducers/deliveryReducer.jsx
@@ -8,9 +8,7 @@ const deliveryReducer = (state = {}, action) => {
   switch (action.type) {
     case GET_VIEWING_DELIVERY: return action.payload
     case SET_VIEWING_DELIVERY: return action.payload
-    case UPDATE_DELIVERY: {
-      return action.payload
-    }
+    case UPDATE_DELIVERY: return action.payload
     default: return state
   }
 }
