ef23f3e074d561a0c1603d0c90367c1cc5bd240b
Margret Soley <margretmsk@gmail.com>
2021-05-10 10:57:54 +0000

    Updating deliver screen

--- Handheld/UtkeyrslukerfiHandheld/src/views/DeliverScreen/index.js
+++ Handheld/UtkeyrslukerfiHandheld/src/views/DeliverScreen/index.js
@@ -15,13 +15,11 @@ const DeliverScreen = ({ route, navigation }) => {
   // - add consecutive screens that depend on checkbox
   // - add serial number to db and switch out count state
   const { delivery } = route.params
-  const [count, setCount] = useState(1)
-  const [barcode, setBarcode] = useState()
   const driver = useSelector(({ login }) => login.driver)
   const [customerComment, setCustomerComment] = useState('')
   const [driverComment, setDriverComment] = useState('')
   const [tableData, setTableData] = useState([])
-  const tableHeaders = ['Sendingarnr.', 'Pakki í sendingu', '']
+  const tableHeaders = ['Pakkanr.', 'Pakki í sendingu']
   const [receiverNotHome, setReveiverNotHome] = useState(false)
 
   // A ref is neccessary since the remove buttons contain callbacks that reference the state at the time of creation otherwise
@@ -39,45 +37,14 @@ const DeliverScreen = ({ route, navigation }) => {
     if (tableData.length === 0) { setTableData(delivery.packages.map((pck, idx) => {
       return {
         barcode: pck.id,
-        package: `${idx+1}/${delivery.packages.length}`,
-        button: <RemoveButton key={pck.id} barcode={pck.id} removeBarcode={removeBarcode} />
+        package: `${idx+1}/${delivery.packages.length}`
       }
     }))}
   }, [])
 
-  // Remove item from table, barcodes need to be unique
-  const removeBarcode = (currentBarcode) => {
-    setTableData(tableDataRef.current.filter(b => b[0] !== currentBarcode))
-  }
-
-  // All packages in current delivery about to be delivered should be scanned
-  const addBarcode = () => {
-    if (!barcode) { return ToastAndroid.showWithGravity('Strikamerki er ekki til staðar', ToastAndroid.LONG, ToastAndroid.TOP) }
-    // Only add packages in this delivery && only add packages once
-    if (delivery.packages.some(p => p.id === barcode) && tableData.every(p => p.barcode !== barcode)) {
-      addToTable(barcode)
-    } else {
-      ToastAndroid.showWithGravity('Rangt strikamerki', ToastAndroid.LONG, ToastAndroid.TOP)
-    }
-    setBarcode('')
-  }
-
-  // Add package to table
-  const addToTable = (barcode) => {
-    setTableData([
-      ...tableData,
-      {
-        barcode: barcode,
-        package: `${count}/${delivery.packages.length}`,
-        button: <RemoveButton key={barcode} barcode={barcode} removeBarcode={removeBarcode} />
-      }
-    ])
-    setCount(count + 1)
-  }
-
   // Navigate to signoff screen where already configured signoff methods will be shown
   const continueWithDelivery = () => {
-    // Set comment in case it was altered and status to 'delivered'
+    // Set comment in case it was altered and change status to 'delivered'
     delivery.driverComment = driverComment
     delivery.status = 3
     delivery.driverID = driver
@@ -87,7 +54,6 @@ const DeliverScreen = ({ route, navigation }) => {
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <BarcodeForm barcode={barcode} setBarcode={setBarcode} enterBarcode={addBarcode} labelText='Strikamerki pakka' />
       <ProductTable tableHeaders={tableHeaders} tableData={tableData} numberOfObjects={2} label='Pakkar í sendingu' />
       <CommentBox label='Athugasemd viðskiptavinar' editable={false} comment={customerComment} setComment={setCustomerComment} />
       <CommentBox label='Athugasemd bílstjóra' editable comment={driverComment} setComment={setDriverComment} />
