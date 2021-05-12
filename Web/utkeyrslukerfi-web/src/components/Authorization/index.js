deabc587ec321cf9a09a6cb46e2a1084e764333f
Mikael Máni Jónsson <mikaeelmani99@gmail.com>
2021-05-12 14:12:48 +0000

    Added Higher order component for authorization

---
+++ Web/utkeyrslukerfi-web/src/components/Authorization/index.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getLogin } from '../../actions/loginActions';
+import Login from '../../components/Login'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function (Component) {
+  const Authorization = () => {
+    const dispatch = useDispatch();
+    const token = useSelector(({ login }) => login.token)
+    const [shouldNotLogin, setShouldNotLogin] = useState(true);
+  
+    useEffect(() => {
+      dispatch(getLogin())
+    })
+  
+    useEffect(() => {
+      const header = document.getElementsByTagName('nav')[0]
+      const container = document.getElementsByClassName('container')[0]
+      if (token === null || token.length === 0) {
+        setShouldNotLogin(false);
+        header.classList.add('hide');
+        container.classList.add('login-container')
+        return;
+      }
+      header.classList.remove('hide');
+      container.classList.remove('login-container')
+      setShouldNotLogin(true);
+    }, [token])
+    return (
+      shouldNotLogin ? <Component /> : <Login />
+    )
+  }
+  return (Authorization)
+}
