1be8911a11db2fa1e33de48afb55d4a6187849ad
Margret Soley <margretmsk@gmail.com>
2021-05-13 19:21:05 +0000

    Add refresh to login

--- Handheld/UtkeyrslukerfiHandheld/src/components/Login/index.js
+++ Handheld/UtkeyrslukerfiHandheld/src/components/Login/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, RefreshControl, ScrollView } from 'react-native'
 import { setLogin } from '../../actions/loginActions'
 import { useDispatch, useSelector } from 'react-redux'
 import DropDownPicker from 'react-native-dropdown-picker'
@@ -11,6 +11,7 @@ const Login = () => {
   const [selected, setSelected] = useState('') // Currently selected driver
   const drivers = useSelector(({ drivers }) => drivers)
   const dispatch = useDispatch()
+  const [refreshing, setRefreshing] = useState(false)
 
   useEffect(() => {
     dispatch(getDrivers())
@@ -21,31 +22,45 @@ const Login = () => {
     dispatch(setLogin(selected))
   }
 
+  const onRefresh = () => {
+    // Get the drivers
+    dispatch(getDrivers())
+    setRefreshing(false)
+  }
+
   return (
-    <View style={styles.mainView}>
-      <View style={styles.dropDown}>
-        <Text style={styles.mainText}>Innskráning</Text>
-        <DropDownPicker
-          items={
-            drivers.map((driver) => {
-              return { label: driver.name, value: driver }
-            })
-          }
-          placeholder='Velja starfsmann...'
-          containerStyle={styles.containerStyle}
-          style={styles.dropDownPickerStyle}
-          itemStyle={styles.itemStyle}
-          dropDownStyle={styles.dropDownStyle}
-          onChangeItem={item => setSelected(item.value)}
-        />
-      </View>
-      <View style={styles.loginButton}>
-        <BasicButton
-          onPressFunction={handleSubmit}
-          buttonText='Skrá inn'
-        />
-      </View>
-    </View>
+      <ScrollView 
+        contentContainerStyle={styles.mainView}
+        refreshControl={
+          <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          />
+        }
+      >
+        <View style={styles.dropDown}>
+          <Text style={styles.mainText}>Innskráning</Text>
+          <DropDownPicker
+            items={
+              drivers.map((driver) => {
+                return { label: driver.name, value: driver }
+              })
+            }
+            placeholder='Velja starfsmann...'
+            containerStyle={styles.containerStyle}
+            style={styles.dropDownPickerStyle}
+            itemStyle={styles.itemStyle}
+            dropDownStyle={styles.dropDownStyle}
+            onChangeItem={item => setSelected(item.value)}
+          />
+        </View>
+        <View style={styles.loginButton}>
+          <BasicButton
+            onPressFunction={handleSubmit}
+            buttonText='Skrá inn'
+          />
+        </View>
+      </ScrollView>
   )
 }
 
