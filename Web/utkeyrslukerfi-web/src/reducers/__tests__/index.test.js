72677e94d7749c231526e795ab82920b02fe2903
ertolv <re_pel@outlook.com>
2021-05-14 09:17:32 +0000

    web test reducers

--- Web/utkeyrslukerfi-web/src/reducers/__tests__/index.test.js
+++ Web/utkeyrslukerfi-web/src/reducers/__tests__/index.test.js
@@ -1,26 +1,49 @@
 import reducer from '../index'
-import getUsers from '../../actions/usersActions'
+import usersReducer from '../userReducer'
+
+
+const initialState = {
+  "deliveries": [],
+  "delivery": {},
+  "login": {
+    "changePassword": false,
+    "role": 4,
+    "token": "",
+  },
+  "pack": [],
+  "packages": [],
+  "user": {},
+  "users": [],
+  "vehicle": [],
+  "vehicles": []
+}
 
 describe('Testing Reducer', () => {
   it('Should return the initial state', () => {
-    let initialState = {
-      "deliveries": [],
-      "delivery": {},
-      "login": {
-        "changePassword": false,
-        "role": 4,
-        "token": "",
-      },
-      "pack": [],
-      "packages": [],
-      "user": {},
-      "users": [],
-      "vehicle": [],
-      "vehicles": []
-    }
-    let users = [
-      {}
-    ]
     expect(reducer(undefined, {})).toEqual({ ...initialState })
+  })
+  it('Shoud test GET_USERS action', () => {
+    let users = [
+      {
+        "id": "1760f20c-788b-4694-19b2-08d913986ac6",
+        "name": "skrifstofa",
+        "email": "skrifstofa@edico.is",
+        "role": 2,
+        "changePassword": true
+      },
+      {
+        "id": "1760f20c-788b-4694-19b2-08d914986ac6",
+        "name": "skrifstofa2",
+        "email": "skrifstofa@edico.is",
+        "role": 2,
+        "changePassword": true
+      }
+    ]
+    const action = {
+      type: 'GET_USERS',
+      payload: users
+    }
+    const result = usersReducer(initialState, action)
+    expect(result.users).toEqual([])
   })
 })
